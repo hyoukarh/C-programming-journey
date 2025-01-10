bf990f0115f0373a25086204ffa2929cf452976d
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-01-10 08:37:51 +0100

    Iniatialization of a variable

---
+++ Exercises/Initialization.c
@@ -0,0 +1,9 @@
+#include <stdio.h> /*include the standard input/output header file (stdio.h)*/
+
+int main() /*Entry point of the program*/
+{
+    int var = 10; /*Variable declaration and initialization*/
+    var = 7; /*Variable reassignment*/
+    printf("%d\n", var); /*Print the value of the variable to the console*/
+    return 0; /*Return 0 to the operating system to indicate successful completion of the program*/
+}
