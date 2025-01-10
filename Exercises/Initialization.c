4f037cd35067d55e6ce53526fdfb2b920c83462f
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-01-10 09:04:04 +0100

    Declaration and intialization of a variable

--- Exercises/Initialization.c
+++ Exercises/Initialization.c
@@ -4,6 +4,11 @@ int main() /*Entry point of the program*/
 {
     int var = 10; /*Variable declaration and initialization*/
     var = 7; /*Variable reassignment*/
-    printf("%d\n", var); /*Print the value of the variable to the console*/
+    int var2, var3, var4; /*Multiple variable declaration*/
+    var2 = var3 = var4 = 5; /*Multiple variable initialization*/
+    printf("%d\n", var); //Print the value of the variable to the console
+    printf("%d", var2); //Print the value of the variable to the console
+    printf("%d", var3); //Print the value of the variable to the console
+    printf("%d", var4); //Print the value of the variable to the console
     return 0; /*Return 0 to the operating system to indicate successful completion of the program*/
 }
