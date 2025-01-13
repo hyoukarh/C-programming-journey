9ee5e6caae16b54f91d5ced1311bc63f8aa34a74
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-01-13 09:35:37 +0100

    use of printf function

---
+++ Exercises/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int two = 2, four = 4, five = 5;
+    int result = (five * four) / two;
+    printf("Hello! I'm Hyouka and I can write simple math equations like this in C: (%d * %d) / %d\n", five, four, two);
+    printf("The result is: %d\n", result);
+    printf("I can also make use of strings to print out string constants on my console.\n");
+    return 0;
+}
