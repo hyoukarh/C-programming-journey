dcd473ea133f14b2f37f230fcbf721ca87bde6fe
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-01-16 09:34:49 +0100

    Char

---
+++ Exercises/Basics/char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char c = 'A'; // Declares a variable c of type char and assigns it the value 'A'.
+    char v = 128; // Declares a variable v of type char and assigns it the value 128.
+    printf("The value of the variable c is: %c\n", c); // Prints the value of the variable c to the console.
+    printf("The value of the variable v is: %c\n", v); // Prints the value of the variable v to the console.
+    return 0;
+}
