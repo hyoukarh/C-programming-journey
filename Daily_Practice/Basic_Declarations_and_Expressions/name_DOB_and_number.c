769f6b140b218af76ba0d27d2be42cd5a1ce2c60
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-02-03 08:21:51 +0100

    Name, DOB, and number

---
+++ Daily_Practice/Basic_Declarations_and_Expressions/name_DOB_and_number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    char name[] = "Robin";
+    char DOB[] = "2000_11_01";
+    char mobile[] = "1156573573684";
+    printf("Name: %s\n", name);
+    printf("DOB: %s\n", DOB);
+    printf("Mobile: %s\n", mobile);
+}
