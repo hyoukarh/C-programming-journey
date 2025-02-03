ac7f0ce223797b5dd5222c05a7a5f4e4aaf733df
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-02-03 08:39:18 +0100

    Constants

---
+++ Exercises/Basics/constant.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define add(x, y) x+y
+#define greater(x, y) if (x < y) \
+                        printf("%d is greater than %d\n", y, x); \
+                    else \
+                        printf("%d is greater than %d\n", x, y);
+
+int main() {
+    greater(7, 8);
+    printf("The value of add(x, y): %d\n", add(2, 3));
+    printf("Todays date is: %s\n", __DATE__);
+    printf("The Time is: %s\n", __TIME__);
+    return 0;
+}
