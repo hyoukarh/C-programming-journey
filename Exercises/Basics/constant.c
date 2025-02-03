1e45ace24cd4be90cbac28a7e7756fee457ef3d8
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-02-03 08:58:08 +0100

    constants

--- Exercises/Basics/constant.c
+++ Exercises/Basics/constant.c
@@ -7,6 +7,8 @@
 
 int main() {
     greater(7, 8);
+    const int var = 10; // constant variable, read only and can not be changed.
+    printf("The value of var: %d\n", var);
     printf("The value of add(x, y): %d\n", add(2, 3));
     printf("Todays date is: %s\n", __DATE__);
     printf("The Time is: %s\n", __TIME__);
