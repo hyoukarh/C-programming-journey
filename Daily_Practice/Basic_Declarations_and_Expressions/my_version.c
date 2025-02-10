3faefe0f82ed913919f6263e728a0f10503684b0
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-02-10 20:39:07 +0100

    update

--- Daily_Practice/Basic_Declarations_and_Expressions/my_version.c
+++ Daily_Practice/Basic_Declarations_and_Expressions/my_version.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("My current version of C is: %d\n", __STDC_VERSION__);
+    printf("My current version of C is: %ld\n", __STDC_VERSION__);
     return 0;
 }
