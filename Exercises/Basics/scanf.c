cb002c1b7e4c32771dea1872555bc4b14cd3d9e7
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-01 15:19:55 +0100

    scanf

--- Exercises/Basics/scanf.c
+++ Exercises/Basics/scanf.c
@@ -6,7 +6,7 @@ int main() {
     char name[50];
 
     printf("Please enter your name, age, and height: ");
-    scanf("%s %d %f", name, &age, &height);
+    scanf("%s %d %f", name, &age, &height); // The scanf function is used to read the input from the user. The %s, %d, and %f are format specifiers that are used to read the string, integer, and float values respectively.
 
     printf("Name: %s\n", name);
     printf("Age: %d\n", age);
