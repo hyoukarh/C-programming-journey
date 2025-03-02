f46cd8a491726e1b02d92b57cb5ca62a53d3ee40
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-02 18:06:21 +0100

    Daily practice

---
+++ Daily_Practice/Basic_Declarations_and_Expressions/Print_F_and_C.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("#####\n#\n#\n#####\n#\n#\n#\n"); // produces a letter F
+    printf("    ####\n  ##     ##\n#\n#\n#\n#\n#\n#\n  ##     ##\n    ####\n");// produces a letter C
+    return 0;
+}
