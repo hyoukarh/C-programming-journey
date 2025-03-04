0d478545c5cd3f4886b0b095a9fc18b93a24d634
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-04 17:11:44 +0100

    daily exercise

--- Daily_Practice/Basic_Declarations_and_Expressions/days_weeks_years.c
+++ Daily_Practice/Basic_Declarations_and_Expressions/days_weeks_years.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int convert;
-    printf("Enter number of days:");
-    scanf("%d", &convert);
+    int totaldays; // Variable to store the users input.
+    printf("Enter number of total-days:"); // prompt to enter total days. 
+    scanf("%d", &totaldays); // Stores the users input to the address.
 
-    int years = convert / 365;
-    int remainder = convert % 365;
-    printf("Years: %d\n", years);
+    int years = totaldays / 365; // Calculates the total number of years.
+    int remainder = totaldays % 365; // Calculates the remainder of the total days.
+    printf("Years: %d\n", years); // Outputs the total number of years.
 
-    int weeks = remainder / 7;
-    int remainder2 = remainder % 7;
-    printf("Weeks: %d\n", weeks);
+    int weeks = remainder / 7; // Calculates the total number of weeks.
+    int remainder2 = remainder % 7; // Calculates the remainder of the weeks.
+    printf("Weeks: %d\n", weeks); // Outputs the total number of weeks.
 
-    int days = remainder2;
-    printf("Days: %d", days);
+    int days = remainder2; 
+    printf("Days: %d", days); // Outputs the total number of days.
     return 0;
 }
