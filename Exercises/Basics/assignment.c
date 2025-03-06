95bb8d2724cb712adb1a36d8521b9c888eff661e
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-06 21:59:08 +0100

    assignment operators

---
+++ Exercises/Basics/assignment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char a = 7;
+    a ^= 5;
+    printf("%d\n", printf("%d\n", a += 3));
+    a -= 2;
+    printf("%d\n", a);
+    a *= 2;
+    printf("%d\n", a);
+    a /= 2;
+    printf("%d\n", a);
+    a %= 2;
+    printf("%d\n", a);
+    return 0;
+}
