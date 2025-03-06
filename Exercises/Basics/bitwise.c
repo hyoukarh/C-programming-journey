1901e679a3d96835605a2d2be28dc745712e3d57
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-06 21:54:27 +0100

    bitwise

--- Exercises/Basics/bitwise.c
+++ Exercises/Basics/bitwise.c
@@ -8,5 +8,9 @@ int main() {
     printf("~%d = %d\n", a, ~a); // performs bitwise NOT operation and reurns the opposite of the number.
     printf("%d << 1 = %d\n", a, a << 1); // performs bitwise left shift operation and shifts the bits to the left by 1.
     printf("%d >> 1 = %d\n", a, a >> 1); // performs bitwise right shift operation and shifts the bits to the right by 1.
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("a = %d, b = %d\n", a, b); // Swapping two numbers using XOR operation.
     return 0;
 }
