2773299f5ff786b78e93a82e4b51eeafc299b1c4
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-07 12:30:48 +0100

    conditionals

--- Exercises/Basics/conditionals.c
+++ Exercises/Basics/conditionals.c
@@ -2,7 +2,7 @@
 
 int main() {
     int a = 10, b = 20, c = 0;
-    int max = (a > b) ? a : b;
+    int max = (a > b) ? a : b; // Ternary operator to find the max number.
     printf("The max number is: %d\n", max);
 
     if (a && c) {
@@ -10,5 +10,9 @@ int main() {
     } else {
         printf("Only a or b has a value of 1\n");
     }
+
+    int var = 75, var2 = 56;
+    int num = sizeof(var) ? (var2 > 23 ? ((var == 75) ? 'A' : 0) : 0) : 0; // Nested ternary operator to find the value of num.
+    printf("%d", num);
     return 0;
 }
