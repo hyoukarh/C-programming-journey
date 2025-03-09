2dfc208f31bf6e984da0d25787eeb92065d3e710
Abdulsalam Yusuf <yusufabdulsalam077@gmail.com>
2025-03-09 15:11:26 +0100

    conditionals

--- Exercises/Basics/conditinals2.c
+++ Exercises/Basics/conditinals2.c
@@ -26,5 +26,36 @@ int main() {
     } else {
         printf("Your grade is F");
     }
+
+    int day;
+    printf("Enter day:\n");
+    scanf("%d", &day);
+    switch (day)
+    {
+    case 1:
+        printf("Day is monday");
+        break;
+    case 2: 
+        printf("Day is Tuesday");
+        break;
+    case 3:
+        printf("Day is Wednesday");
+        break;
+    case 4:
+        printf("Day is Thursday");
+        break;
+    case 5:
+        printf("Day is Friday");
+        break;
+    case 6:
+        printf("Day is Saturday");
+        break;
+    case 7:
+        printf("Day is Sunday");
+        break;
+    default:
+        printf("Invalid day");
+        break;
+    }
     return 0;
 }
